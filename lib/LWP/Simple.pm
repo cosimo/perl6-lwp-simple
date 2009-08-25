4b81701bd3cbbfdb3c865c885373a7ff9c3add1e
cosimo <cosimo@cpan.org>
2009-08-25 14:44:02 +0200

    Tabs are 4 spaces now

--- lib/LWP/Simple.pm
+++ lib/LWP/Simple.pm
@@ -8,48 +8,49 @@ use IO::Socket::INET;
 
 class LWP::Simple {
 
-	method get (Str $url) {
+    method get (Str $url) {
 
-		return unless $url;
+        return unless $url;
 
-		my $proto;
-		my $hostname;
-		my ($path, @path);
-		my $port;
+        my $proto;
+        my $hostname;
+        my ($path, @path);
+        my $port;
 
-		($proto, $hostname, @path) = $url.split(/\/+/);
-		$proto .= chop;
+        ($proto, $hostname, @path) = $url.split(/\/+/);
+        $proto .= chop;
 
-		$path = '/' ~ @path.join('/');
+        $path = '/' ~ @path.join('/');
 
-		($hostname, $port) = $hostname.split(':');
-		if ! $port {
-			$port = 80;
-		}
+        ($hostname, $port) = $hostname.split(':');
+        if ! $port {
+            $port = 80;
+        }
 
-		#say 'hostname:', $hostname;
-		#say 'port:', $port;
-		#say 'path:', $path;
-		#say 'proto:', $proto;
+        #say 'hostname:', $hostname;
+        #say 'port:', $port;
+        #say 'path:', $path;
+        #say 'proto:', $proto;
 
-		my $sock = IO::Socket::INET.new;
-		$sock.open($hostname, $port);
-		$sock.send(
-			"GET {$path} HTTP/1.1\r\n"
-			~ "Host: {$hostname}\r\n"
-			~ "Accept: */*\r\n"
-			~ "User-Agent: Perl6-LWP-Simple/0.01\r\n"
-			~ "Connection: close\r\n\r\n"
-		);
-		my $page = $sock.recv();
-		$sock.close();
-		
-		return $page;
-	}
+        my $sock = IO::Socket::INET.new;
+        $sock.open($hostname, $port);
+        $sock.send(
+            "GET {$path} HTTP/1.1\r\n"
+            ~ "Host: {$hostname}\r\n"
+            ~ "Accept: */*\r\n"
+            ~ "User-Agent: Perl6-LWP-Simple/0.01\r\n"
+            ~ "Connection: close\r\n\r\n"
+        );
 
-	method getprint (Str $url) {
-		say self.get($url);
-	}
+        my $page = $sock.recv();
+        $sock.close();
+
+        return $page;
+    }
+
+    method getprint (Str $url) {
+        say self.get($url);
+    }
 
 }
 
